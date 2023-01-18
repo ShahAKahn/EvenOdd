44f7dc94142e046d4b5dc7ad7b9cddea38499d5d
Shaheer Khan <khanpsha@corp.contoursoftware.net>
2023-01-18 14:35:15 +0500

    EvenOdd

--- Evennumberchecker.js
+++ Evennumberchecker.js
@@ -5,13 +5,15 @@ const number = prompt("Enter a number: ");
 //Check if the number is odd
 if(number % 2 == 0) 
 {
-    document.write("The number is even. user");
+    document.write("The number is even, User");
+    console.log("The number is even, User");
 }
 
 //If any other number
 else
 {
-    document.write("The number is not even.user ");
+    document.write("The number is not even, User ");
+    console.log("The number is not even, User");
 }
 }
 
